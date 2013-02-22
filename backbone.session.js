760418ffd1ecc7a695cc39fdb99ac3b3048b2a96
Makis Tracend <makis.tracend@gmail.com>
2013-02-22 03:49:22 -0800

    syncing local store with remote

--- backbone.session.js
+++ backbone.session.js
@@ -8,45 +8,78 @@ if(typeof APP == "undefined"){ var APP = { }; }
  
 APP.Session = Backbone.Model.extend({
 	url: "/session", 
-	//localStorage: new Store("session"),
-	defaults: {
-		id: 1, // this will make the access account unique on subsequent visits
-		local: false
+	defaults : {
+		auth: false,
+		updated: false
+	}, 
+	options: {
+		local: true, 
+		remote: true,
+		persist: false 
 	},
 	initialize: function( model, options ){
+		console.log("options", this.options);
+		// fallbacks
+		if( typeof options == "undefined" ) options = {};
 		// pick a persistance solution 
-		if(typeof sessionStorage != "undefined" && sessionStorage !== null){
+		if( !this.get("persist") && typeof sessionStorage != "undefined" && sessionStorage !== null ){
 			// choose localStorage
 			this.store = this.sessionStorage;
-		} else if(typeof localStorage != "undefined" && localStorage !== null){
+		} else if( typeof localStorage != "undefined" && localStorage !== null ){
 			// choose localStorage
 			this.store = this.localStorage;
 		} else {
 			// otherwise we need to store data in a cookie
 			this.store = this.cookie;
 		}
-
-		// setting up custom domain for session
-		//if( !_.isUndefined(options.url) ) this.url = options.url;
-		this.fetch();
+		// load local
+		try {
+			this.set( JSON.parse( this.store.get("session") ) );
+		} catch ( exc ){
+			// exception
+		}
+		// parse options
+		if( !_.isUndefined(options.url) ) this.url = options.url;
+		
+		// calling remote domain for session
+		if( this.options["remote"] ) this.fetch();
 		
 		//this.bind("change",this.cache);
 		this.bind("error", this.error);
 	}, 
+	/*
+	sync: function(method, model, options) {
+		if(typeof options == "undefined") options = {};
+		// intercept local store actions
+		switch(method){
+			case "read":
+				model.parse( JSON.parse( this.store.get("session") ) );
+			break;
+			case "save":
+				this.store.set("session", model.toJSON() );
+			break;
+		}
+		// end now if there's no remote
+		if( !this.get("remote") ){
+			// work locally
+			return;
+		}
+		
+		options.success = function( model, resp, options ){
+			console.log("SUCCESSS!", method );
+		};
+		
+		return Backbone.sync.call(this, method, model, options);
+	}, 
+	*/
 	parse: function( data ) {
 		
-		// create the components collection
-		if ( _.isEmpty( data ) ){
-			/*
-			//get the latest version from localStorage
-			if( _.isNull( this.localStorage.find( this ) ) ){ 
-				// redirect the user to register
-			} else { 
-				data = this.localStorage.find( this );
-				this.set({local: true});
-			}
-			*/
+		// add updated flag
+		if( typeof data.updated == "undefined" ){
+			data.updated = ( new Date() ).getTime();
 		}
+		// cache locally
+		this.store.set("session", JSON.stringify( data ) );
 		
 		return data;
 		
@@ -67,7 +100,7 @@ APP.Session = Backbone.Model.extend({
 	}, 
 	// if data request fails request offline mode. 
 	error: function( model, req, options, error ){
-		console.log( req );
+		console.log( error );
 	},
 	sessionStorage : {
 		get : function( name ) {
